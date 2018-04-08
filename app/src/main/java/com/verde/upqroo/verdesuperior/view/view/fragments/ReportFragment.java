b50f51948e95010c12f93e27cc8b2ea74b55bf47
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-04-08 00:36:55 -0500

    Diseño Reportes.

--- app/src/main/java/com/verde/upqroo/verdesuperior/view/view/fragments/ReportFragment.java
+++ app/src/main/java/com/verde/upqroo/verdesuperior/view/view/fragments/ReportFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.verde.upqroo.verdesuperior.R;
 
@@ -24,7 +28,28 @@ public class ReportFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_report, container, false);
+
+        ImageButton adjuntarButton = (ImageButton) view.findViewById(R.id.adjuntarButton);
+        Button sendReport = (Button) view.findViewById(R.id.sendReport);
+
+        adjuntarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Adjuntado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        sendReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "COMPRUEBA EL MENSAJE", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+
     }
 
 }
