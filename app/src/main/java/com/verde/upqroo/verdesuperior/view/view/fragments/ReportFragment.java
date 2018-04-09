a7dff88673f8b62dfb21bb1265aad8d1256ae106
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-04-08 21:33:20 -0500

    Reportes - Funciona, pero no añade destino.

--- app/src/main/java/com/verde/upqroo/verdesuperior/view/view/fragments/ReportFragment.java
+++ app/src/main/java/com/verde/upqroo/verdesuperior/view/view/fragments/ReportFragment.java
@@ -11,7 +11,6 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
