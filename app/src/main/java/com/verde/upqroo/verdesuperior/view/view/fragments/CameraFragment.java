315730d89df6f38cfb34356017c943f18c358416
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-04-09 18:35:26 -0500

    Diseño de "Detalle Plantas" o "Planta Escaneada"

--- app/src/main/java/com/verde/upqroo/verdesuperior/view/view/fragments/CameraFragment.java
+++ app/src/main/java/com/verde/upqroo/verdesuperior/view/view/fragments/CameraFragment.java
@@ -1,19 +1,40 @@
 package com.verde.upqroo.verdesuperior.view.view.fragments;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.util.SparseArrayCompat;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.verde.upqroo.verdesuperior.R;
+
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CameraFragment extends Fragment {
 
+    SurfaceView cameraView;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    SurfaceHolder surfaceHolder;
 
     public CameraFragment() {
         // Required empty public constructor
@@ -23,8 +44,12 @@ public class CameraFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+
+        final View view = inflater.inflate(R.layout.fragment_camera, container, false);
+
+
+
+        return view;
     }
 
 }
---
+++ app/src/main/java/com/verde/upqroo/verdesuperior/view/view/fragments/PlanFragment.java
@@ -0,0 +1,33 @@
+package com.verde.upqroo.verdesuperior.view.view.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.verde.upqroo.verdesuperior.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PlanFragment extends Fragment {
+
+
+    public PlanFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_plan, container, false);
+
+        return view;
+    }
+
+}
