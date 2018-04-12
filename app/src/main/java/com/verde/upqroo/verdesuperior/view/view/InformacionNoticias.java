cd246b5f0c3227b37417ff65ac5741dc5ce46609
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-04-12 01:17:02 -0500

    Añadido Diseño de Noticias Detalladas

---
+++ app/src/main/java/com/verde/upqroo/verdesuperior/view/view/InformacionNoticias.java
@@ -0,0 +1,15 @@
+package com.verde.upqroo.verdesuperior.view.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.verde.upqroo.verdesuperior.R;
+
+public class InformacionNoticias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacion_noticias);
+    }
+}
